ae812feec83e5ff145a0cb0a6129ed744bf00ac4
Dylan Thinnes <dylan.thinnes@protonmail.com>
2020-12-09 23:45:15 +0000

    Token OOPification of Day 7 Ruby impl

--- 7/ruby/sol1.rb
+++ 7/ruby/sol1.rb
@@ -1,66 +1,72 @@
 #!/usr/bin/env ruby
-$dict = {}
-
-def get_node(id)
-  if not $dict[id] then
-    $dict[id] = {
-      :id => id,
-      :children => {},
-      :parents => []
-    }
+class NodeMap
+  def initialize
+    @dict = {}
   end
 
-  return $dict[id]
-end
-
-STDIN.read.split("\n").each do |line|
-  parent, *children = line.split(/ bags contain no other bags\.| bags contain | bags?, | bags\./)
-  parent_node = get_node parent
-
-  children.each do |str|
-    match = str.match(/(?<qty>\d+) (?<id>\w+ \w+)/)
-    child_node = get_node match[:id]
-
-    child_node[:parents].append parent_node
-    parent_node[:children][match[:id]] = {
-      :id => match[:id],
-      :qty => match[:qty].to_i,
-      :node => child_node
-    }
-  end
-end
-
-def find_parents(child_id)
-  def helper(arr, child)
-    parents = child[:parents]
-
-    parents.each do |parent|
-      arr.append parent[:id]
-      helper arr, parent
+  def get_node(id)
+    if not @dict[id] then
+      @dict[id] = {
+        :id => id,
+        :children => {},
+        :parents => []
+      }
     end
 
-    return arr
+    return @dict[id]
   end
 
-  return helper([], get_node(child_id)).uniq!.length
-end
+  def insert_line(line)
+    parent, *children = line.split(/ bags contain no other bags\.| bags contain | bags?, | bags\./)
+    parent_node = get_node parent
 
-def count_children(parent)
-  def helper(parent)
-    children = parent[:children]
+    children.each do |str|
+      match = str.match(/(?<qty>\d+) (?<id>\w+ \w+)/)
+      child_node = get_node match[:id]
 
-    total = 0
+      child_node[:parents].append parent_node
+      parent_node[:children][match[:id]] = {
+        :id => match[:id],
+        :qty => match[:qty].to_i,
+        :node => child_node
+      }
+    end
+  end
 
-    children.each do |_, child|
-      total += child[:qty]
-      total += child[:qty] * helper(child[:node])
+  def find_parents(child_id)
+    def helper(arr, child)
+      parents = child[:parents]
+
+      parents.each do |parent|
+        arr.append parent[:id]
+        helper arr, parent
+      end
+
+      return arr
     end
 
-    return total
+    return helper([], get_node(child_id)).uniq!.length
   end
 
-  return helper get_node "shiny gold"
+  def count_children(parent)
+    def helper(parent)
+      children = parent[:children]
+
+      total = 0
+
+      children.each do |_, child|
+        total += child[:qty]
+        total += child[:qty] * helper(child[:node])
+      end
+
+      return total
+    end
+
+    return helper get_node parent
+  end
 end
 
-puts find_parents "shiny gold"
-puts count_children "shiny gold"
+map = NodeMap.new
+STDIN.read.split("\n").each do |line| map.insert_line(line) end
+puts map.find_parents "shiny gold"
+puts map.count_children "shiny gold"
